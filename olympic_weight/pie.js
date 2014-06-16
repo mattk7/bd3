899c2353b490e18b2a7af410a052fedcb946a0ff
qrawl <pierre@prinetti.it>
2014-06-16 09:23:45 +0200

    pies must go

--- olympic_weight/pie.js
+++ olympic_weight/pie.js
@@ -1,5 +1,3 @@
-
-
 var pie_width = 300,
     pie_height = 200,
     pie_radius = Math.min(pie_width, pie_height) / 2
@@ -9,7 +7,7 @@ var pie_color = d3.scale.ordinal()
 
 var pie_arc = d3.svg.arc()
     .outerRadius(pie_radius - 10)
-    .innerRadius(pie_radius*0.5)
+    .innerRadius(pie_radius * 0.5)
 
 var pie = d3.layout.pie()
     .value(function(d, i) { return d.pop })
@@ -23,24 +21,31 @@ var svg = d3.select("div#widget_container").append("svg")
 function make_pie(pie_data) {
     console.log('pie_data:', pie_data)
     pie_array = []
+    if (pie_data.length > 0){
     for(i=0;i<pie_data[0].values.length;i++){
         var sex = pie_data[0].values[i].key
         var population = pie_data[0].values[i].values.length
         pie_array.push({gender:sex, pop:population})
-    }
+    }}
     console.log('pie_array', pie_array)
+
   var g = svg.selectAll(".arc")
       .data(pie(pie_array))
-    .enter().append("g")
-      .attr("class", "arc");
-        
-  g.append("path")
+  
+  
+  g.enter().append("g")
+      .attr("class", "arc")
+      .append("path")
       .attr("d", pie_arc)
-      .style("fill", function(d) { return pie_color(d.data.gender) })
+      .style("fill", function(d) { return pie_color(d.data.gender) });
 
-  g.append("text")
+  g.exit().remove();
+
+  /*  
+  .append("text")
       .attr("transform", function(d) { return "translate(" + pie_arc.centroid(d) + ")" })
       .attr("dy", ".35em")
       .style("text-anchor", "middle")
       .text(function(d) { return d.data.Sex; });
+*/
 }
