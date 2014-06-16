275d434032ea86ba75937ed5194575bfad1dea47
qrawl <pierre@prinetti.it>
2014-06-16 11:09:27 +0200

    multipie test

--- olympic_weight/pie.js
+++ olympic_weight/pie.js
@@ -10,9 +10,14 @@ var pie_arc = d3.svg.arc()
     .innerRadius(pie_radius * 0.5)
 
 var pie = d3.layout.pie()
-    .value(function(d, i) { return d.pop })
+    .value(function(d, i) { return d.values.length })
 
-var svg = d3.select("div#widget_container").append("svg")
+var svg = d3.select("div#widget_container").append('div')
+    .attr('class', 'single_widget')
+    .attr('id', function(d,i){
+        return i
+    })
+    .append("svg")
     .attr("width", pie_width)
     .attr("height", pie_height)
   .append("g")
@@ -20,32 +25,38 @@ var svg = d3.select("div#widget_container").append("svg")
 
 function make_pie(pie_data) {
     console.log('pie_data:', pie_data)
-    pie_array = []
-    if (pie_data.length > 0){
-    for(i=0;i<pie_data[0].values.length;i++){
-        var sex = pie_data[0].values[i].key
-        var population = pie_data[0].values[i].values.length
-        pie_array.push({gender:sex, pop:population})
-    }}
-    console.log('pie_array', pie_array)
+//    pie_array = []
+//    if (pie_data.length > 0){
+//        for(i=0;i<pie_data[0].values.length;i++){
+//            var sex = pie_data[0].values[i].key
+//            var population = pie_data[0].values[i].values.length
+//            pie_array.push({gender:sex, pop:population})
+//        }
+//    }
+//    console.log('pie_array', pie_array)
 
-  var g = svg.selectAll(".arc")
-      .data(pie(pie_array))
+    var grPie = svg.selectAll('g.pie')
+        .data(pie_data)
+        .enter()
+        .append('g')
+        .attr('class', 'pie')
+        
+  grPie.selectAll("path")
+      .data(function(d){
+          return pie( d.values )
+      })
   
-  
-  g.enter().append("g")
-      .attr("class", "arc")
+  grPie.enter()
       .append("path")
       .attr("d", pie_arc)
-      .style("fill", function(d) { return pie_color(d.data.gender) });
-
-  g.exit().remove();
-
-  /*  
+      .style("fill", function(d){return pie_color(d.data.gender)})
+/*
   .append("text")
       .attr("transform", function(d) { return "translate(" + pie_arc.centroid(d) + ")" })
       .attr("dy", ".35em")
       .style("text-anchor", "middle")
-      .text(function(d) { return d.data.Sex; });
+      .text(function(d) { return d.data.Sex; })
 */
+  grPie.exit().remove();
+
 }
